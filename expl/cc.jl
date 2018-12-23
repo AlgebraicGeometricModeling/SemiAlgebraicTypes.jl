93af083a6bb52a3f8308b436f954067ce969cd59
mourrain <bernard.mourrain@inria.fr>
2018-12-23 19:59:15 +0100

    add data, expl

---
+++ expl/cc.jl
@@ -0,0 +1,27 @@
+using SemiAlgebraicTypes
+using Axl
+
+datadir="../data/"
+
+include("../src/GSpline.jl")
+include("../src/subdiv.jl")
+
+#m = axlread(joinpath(datadir,"forprojection/cube.axl"))[1]
+#m = axlread(joinpath(datadir,"sbd/hexagon.axl"))[1]
+m = axlread(joinpath(datadir,"sbd/square1.axl"))[1]
+
+m[:color] = Color(0,0,255)
+m[:size] = 0.25
+
+hm = hmesh(m)
+hm[:color] = Color(255,0,0)
+hm[:size] = 0.25
+
+# @axlview hm
+
+cc_subdivide!(hm,2)
+
+#cc_subdivide!(hm)
+
+@axlview hm, m
+
