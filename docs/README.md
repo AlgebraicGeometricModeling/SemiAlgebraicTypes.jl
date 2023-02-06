d47e956012f8b42aff964234253ae5ce71a2989d
mourrain <bernard.mourrain@inria.fr>
2023-02-06 12:01:51 +0100

    add doc

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+The package `SemiAlgebraicTypes.jl` provides implementation of semialgebraic sets such as points, spheres, lines cones, bspline functions, ...
+
+
+[![](https://img.shields.io/badge/docs-latest-blue.svg)](https://AlgebraicGeometricModeling.github.io/SemiAlgebraicTypes.jl)
--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,5 @@
 using Documenter, SemiAlgebraicTypes
 
-dir = "mrkd"
 Expl = []; 
 Code = ["code.md", "mesh.md"];
 
@@ -8,23 +7,21 @@ makedocs(
          sitename = "SemiAlgebraicTypes.jl",
          authors = "B. Mourrain",
          modules = [SemiAlgebraicTypes],
-         build = "html",
-         source = dir,
+#         build = "html",
+         source = "mrkd",
          pages = Any[
                      "Home" => "index.md",
                      "Example" => Expl,
                      "SemiAlgebraic types" => Code
                      ],
-         repo = "https://gitlab.inria.fr/AlgebraicGeometricModeling/SemiAlgebraicTypes.jl/tree/master",
+ #        repo = "https://gitlab.inria.fr/AlgebraicGeometricModeling/SemiAlgebraicTypes.jl/tree/master",
          doctest = false
          )
 
-#deploydocs(
-#           deps = Deps.pip("mkdocs", "python-markdown-math"),
-#           repo = "gitlab.inria.fr/AlgebraicGeometricModeling/SemiAlgebraicTypes.jl.git",
-#           target = "site",
-#           julia  = "0.6",
-#           osname = "osx",
-#           deps = nothing,
-#           make = nothing
-#           )
+deploydocs(
+    root = "../docs",
+    repo = "github.com/AlgebraicGeometricModeling/SemiAlgebraicTypes.jl.git",
+    devbranch = "master",
+    push_preview = true,
+)
+
