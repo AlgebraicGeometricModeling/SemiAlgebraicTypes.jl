ce35ad46c0f8e2a6cced01beda8056495fc9afde
mourrain <bernard.mourrain@inria.fr>
2023-02-22 15:44:46 +0100

    update doc

--- src/bspline.jl
+++ src/bspline.jl
@@ -79,10 +79,14 @@ macro bs_er_scale(bvals, knots, mid, num)
 end
 
 """
-``` knots(d,N,μ,a,b) ```
+``` 
+knots(d,N,μ,a,b) 
+```
 
-    Compute the knot sequence of degree d, of multiplicity μ with N equally spaced subintervals between a and b.
-    The default values are  μ=1, a=1.0, b=1.0.
+Compute the knot sequence of degree d, of multiplicity μ with N equally spaced subintervals between a and b.
+
+
+The default values are  μ=1, a=1.0, b=1.0.
 """
 function knots(d,N, μ=1, a=0.0, b=1.0)
     kn = [a]
